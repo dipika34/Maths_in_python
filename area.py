00da581b7b66760b68ed6ea37fdc2e952d6670fa
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-10 15:33:55 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,9 @@
+def area(radius):
+    
+    return(3.14*radius**2)
+
+radius = int(input("Enter the radius of the circle :"))
+
+temp = area(radius)
+
+print("The area of a circle is :",temp,"cm2")
