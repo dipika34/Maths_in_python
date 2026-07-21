984a39b8a77b2cd3ff4a50a674d641759919cb97
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-21 06:46:01 +0530

    Create PythagorasTheorem.py

---
+++ PythagorasTheorem.py
@@ -0,0 +1,9 @@
+import math
+perpendicular  = int(input("Enter the perpendicular :"))
+
+base = int(input("Enter the base : "))
+
+hypotenuse = math.sqrt((math.pow(perpendicular,2)+math.pow(base,2)))
+
+print("The hypotenuse of the right triangle is ",hypotenuse)
+
