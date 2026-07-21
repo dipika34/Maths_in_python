f35407edd8cfc240a1dcda584a7be3cbe2f793a0
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-21 06:32:25 +0530

    Create Area_of_trapezium.py

---
+++ Area_of_trapezium.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value of b : "))
+height = int(input("Enter the height of the trapezium:"))
+area_of_trapezium = (a  +  b)*height/2
+print("The area of the trapezium is : ",area_of_trapezium,"cm^2")
